7c8530dbcf9c9eb53573aee60e044a5d1945dd04
mmgcnerds <114290815+mmgcnerds@users.noreply.github.com>
2022-10-24 17:23:19 -0300

    voltando para o inicio

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
 console.log("Rodando o Sistema de Gerenciamento de Jogos")
 
-console.log("aplicação está se conectando ao banco de dados")
-
-console.log("Enviando para o GitHub")
-
-console.log ("Esse arquivo vai voltar a versão anterir e isso é um teste")
