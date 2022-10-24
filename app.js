13b37caa86e6705cece584de3b0f0a488abeb11e
mmgcnerds <114290815+mmgcnerds@users.noreply.github.com>
2022-10-24 20:32:38 -0300

    Arrumando a , para . na linha 5 do js

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ console.log("Rodando o Sistema de Gerenciamento de Jogos")
 
 console.log("aplicação está se conectando ao banco de dados")
 
-console,log("Enviando para o GitHub")
+console.log("Enviando para o GitHub")
